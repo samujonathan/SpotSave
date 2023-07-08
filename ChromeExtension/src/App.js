3d3159b777ee7cd963aa77500cc553ec4a618f83
Jonathan <jonathanjobsamuel@gmail.com>
2023-07-08 18:19:14 +0530

    Adding BoilerPlate

---
+++ ChromeExtension/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test.js'
+function App() {
+  return (
+   <Test/>
+  );
+}
+
+export default App;
---
+++ ChromeExtension/src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      <p>guitar</p>
+    </div>
+  )
+}
+
+export default Test
